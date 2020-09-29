3f56c5e08c5e25323068a3890d9ec378451e7966
Ciaran <ciaran_toner@icloud.com>
2020-09-29 20:23:32 +0100

    This is for the mobile navbar menu functionality.

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const mobileMenuButton = document.querySelector(".mobile-menu");
+const mobileMenu = document.querySelector(".nav-links");
+const mobileMenuLinks = document.querySelectorAll(".nav-links li");
+
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("open");
+});
