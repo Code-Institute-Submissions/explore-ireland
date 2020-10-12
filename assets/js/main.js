983f924d6a43bd14d3d78316446e0d08f14bc94b
Ciaran <ciaran_toner@icloud.com>
2020-10-12 19:26:00 +0100

    Added event listner for mobile menu button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,13 @@
 const mobileMenuButton = document.querySelector(".mobile-menu");
 const mobileMenu = document.querySelector(".nav-links");
 const mobileMenuLinks = document.querySelectorAll(".nav-links li");
+const MobileMenuimg = document.querySelector(".mobile-menu-img")
 
 mobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("open");
+});
+
+mobileMenuimg.addEventListener("click", () => {
+  ;
 });
 
