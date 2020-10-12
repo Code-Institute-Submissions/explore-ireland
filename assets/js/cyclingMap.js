429ab78dbee7a6883847b02bab00e9c577adcb05
Ciaran <ciaran_toner@icloud.com>
2020-10-12 19:25:21 +0100

    Added objects for google maps api.

--- assets/js/cyclingMap.js
+++ assets/js/cyclingMap.js
@@ -1,1 +1,12 @@
-var cyclingLocation = [{'name':'Ballyhoura - Castlepook Loop','lat':52.3168655,'lng':-8.5028604 }];
+let cyclingLocation = [
+  {'name':'Achill Cycle Hub','lat':53.97610111,'lng':-10.08038879},
+  {'name':'Ballintotis- Castlepook Loop','lat':51.911149,'lng':-8.092401 },
+  {'name':'Beara Way Cycling Route','lat':51.75020745,'lng':-9.549238756  },
+  {'name':'Clifden Cycle Hub','lat':53.48742769,'lng':-10.01909941 },
+  {'name':'Donegal Cycle Route','lat':54.652177,'lng':-8.111556 },
+  {'name':'Doolin Cycle Hub','lat':53.00525867,'lng':-9.388025676 },
+  {'name':'East Kilkenny Cycle Route','lat':52.649863,'lng':-7.249158 },
+  {'name':"Lough O'Flynn Cycle Route",'lat':53.741832,'lng':-8.633925 },
+  {'name':'Mullingar Cycle Hub','lat': 53.522007,'lng':-7.34634 },
+  {'name':'North Kilkenny Cycle Loop','lat': 52.649863,'lng':-7.249158 }
+];
